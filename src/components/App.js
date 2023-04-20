73b9da95a1f4dcedb1ab93de8e65b2836bc9ee5d
ivymarubu <ivymarubu@gmail.com>
2023-04-20 15:40:52 +0300

    first finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
 import React from "react";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 import blogData from "../data/blog";
+
 
 console.log(blogData);
 
+
 function App() {
+  const articles = [
+    {
+      title: "First article",
+      date: "April 1, 2023",
+      preview: "This is the preview for the first article.",
+    },
+    {
+      title: "Second article",
+      date: "April 2, 2023",
+      preview: "This is the preview for the second article.",
+    },
+    {
+      title: "Third article",
+      date: "April 3, 2023",
+      preview: "This is the preview for the third article.",
+    },
+  ];
+
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+
+    <div>
+      <Header name="Projects Based Blog" />
+      <About
+        image="https://example.com/my-image.jpg"
+        aboutText="This is some information about my blog."
+         />
+      <ArticleList articles={articles} />
     </div>
+  
   );
 }
+
 
 export default App;
